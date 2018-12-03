7807409b9bab11b7b1776a9f55b6d3fa51800cb4
Lexden12 <lexden.s@gmail.com>
2018-12-03 00:06:31 -0800

    Import tests, Quick Run, and Multi-String run. Add the ability to import a text file for running tests. Add the ability to opt for a no-GUI quick run option. Add the ability to opt for a Multi-String run GUI option.

--- src/turing/machine/Simulator.java
+++ src/turing/machine/Simulator.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2018 Alex "Lexden" Schendel <lexden.s@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package turing.machine;
@@ -10,28 +21,49 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.ScrollPaneConstants;
 
 /**
  *
  * @author Alex "Lexden" Schendel
  */
 public class Simulator {
+    private final static String PATH = System.getProperty("user.dir") + "\\testFiles\\";
     public static void main(String[] args) {
-        String path = "C:\\Users\\alexi\\Documents\\NetBeansProjects\\Turing Machine\\test\\";
-        String file = JOptionPane.showInputDialog("Enter the path to the csv to the TM you wish to simulate(for the test files, just enter test1 etc.):");
+        int help = JOptionPane.showConfirmDialog(null, "Welcome to the Turing Machine simulator! Would you like to read the instructions?", "Turing Machine Simulator", JOptionPane.YES_NO_OPTION);
+        String file = "";
+        if(help == 0){
+            JOptionPane.showMessageDialog(null, "To get started, you will need to get a Turing Machine in the proper format.");
+            JOptionPane.showMessageDialog(null, "You can use one of the test files included by simply typing \"test#\" where the \"#\" is the number of the file.");
+            JOptionPane.showMessageDialog(null, "You can find more information about the expected format to create your own Turing Machine in the README.");
+            file = JOptionPane.showInputDialog("Try entering the path to the TM now:");
+        }
+        else
+            file = JOptionPane.showInputDialog("Enter the path to the csv to the TM you wish to simulate(for the test files, just enter test1 etc.):");
         TuringMachine TM;
         if(file.contains("\\"))
             TM = new TuringMachine(file);
         else
-            TM = new TuringMachine(path+file+".csv");
-        String in = JOptionPane.showInputDialog("Enter the string you wish to test or enter the path to the text file:");
-        int gui = JOptionPane.showConfirmDialog(null, "Would you like to use the GUI?", "Turing Machine Simulator", JOptionPane.YES_NO_OPTION);
+            TM = new TuringMachine(PATH+file+".csv");
+        String in;
+        if(help == 0){
+            JOptionPane.showMessageDialog(null, "Great! Now it's time to test your Turing Machine.");
+            JOptionPane.showMessageDialog(null, "To test it, you need to enter a string or a path to a text file.");
+            JOptionPane.showMessageDialog(null, "You can also use the test inputs that came with the program by again typing \"test#\" where the \"#\" is the number of the file.");
+            in = JOptionPane.showInputDialog("Try entering the test string or path to source file:");
+        }
+        else
+            in = JOptionPane.showInputDialog("Enter the string you wish to test or enter the path to the text file:");
+        int gui;
+        if(help == 0){
+            JOptionPane.showMessageDialog(null, "Lastly, there are two modes you can run this simulator in. GUI (graphics) or no GUI (quick-run).");
+            JOptionPane.showMessageDialog(null, "The GUI has nice features such as animating the states and transitions and tape of the Turing Machine as you step through the input string.");
+            JOptionPane.showMessageDialog(null, "The no GUI version (quick-run) is designed to immediately run all the inputs to the TM and give you back the results (accept or reject) immediately.");
+            gui = JOptionPane.showConfirmDialog(null, "Knowing that, would you like to run it in GUI mode?", "Turing Machine Simulator", JOptionPane.YES_NO_OPTION);
+        }
+        else
+            gui = JOptionPane.showConfirmDialog(null, "Would you like to use the GUI?", "Turing Machine Simulator", JOptionPane.YES_NO_OPTION);
         if(gui == 0){
             if(in.contains("\\") || in.startsWith("test")){
                 ArrayList<String> strings = parser(in);
@@ -45,7 +77,7 @@ public class Simulator {
             }
         }
         else{
-            if(in.contains("\\")){
+            if(in.contains("\\") || in.startsWith("test")){
                 ArrayList<String> strings = parser(in);
                 if(strings == null)
                     return;
@@ -65,8 +97,7 @@ public class Simulator {
     
     public static ArrayList<String> parser(String file){
         if(file.startsWith("test")){
-            String path = "C:\\Users\\alexi\\Documents\\NetBeansProjects\\Turing Machine\\test\\";
-            file = path+"input_"+file+".txt";
+            file = PATH+"input_"+file+".txt";
         }
         File f = new File(file);
         Scanner s;
--- src/turing/machine/TMCanvas.java
+++ src/turing/machine/TMCanvas.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2018 Alex "Lexden" Schendel <lexden.s@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package turing.machine;
@@ -22,7 +33,7 @@ public class TMCanvas extends Canvas{
     private TuringMachine TM;
     ArrayList<TMState> states;
     private final int STATE_SIZE = 100;
-    private final int RADIUS = 350;
+    private final int RADIUS = 300;
     private float scale;
     private TMState currentState;
     private boolean isStart;
@@ -96,7 +107,7 @@ public class TMCanvas extends Canvas{
     }
     
     private int getStateY(int i){
-        return (int)(RADIUS * scale * Math.sin((i * Math.PI * 2)/states.size())) + getHeight()/2;
+        return (int)(RADIUS * scale * Math.sin((i * Math.PI * 2)/states.size())) + getHeight()*7/16;
     }
     
     public TMState step(){
